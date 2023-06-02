f0aea606b387fb9052ad6835e86d3f4ddfa50ee7
Pull3D <paulo.barradas@gmail.com>
2023-06-02 15:38:08 +0100

    Autocommit from 2023-06-02 15:38:08
    Klipper on commit: 0539e9f
    
    Moonraker on commit: d623163
    
    Mainsail version: v2.5.1

--- autocommit.sh
+++ autocommit.sh
@@ -77,7 +77,7 @@ push_config(){
   git add .
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push origin $branch --force
+  git push origin $branch
 }
 
 grab_version
